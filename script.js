99a1f83ec9d4279dfff446ccf526114eb1d490a9
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2023-07-29 16:36:40 +0530

    Added reset feature

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const player2ScoreElement = document.getElementById('player2-score');
 const rollDiceButton = document.getElementById('roll-dice');
 const winnerElement = document.getElementById('winner');
 const winnerNameElement = document.getElementById('winner-name');
+const resetButton = document.querySelector("#reset ")
 const image1 = document.getElementById('img1')
 const image2 = document.getElementById('img2')
 
-let num1 = 0;
-let num2 = 0;
+var num1 = 0;
+var num2 = 0;
 
 function rollDice() {
   const player1Roll = Math.floor(Math.random() * 6)+1;
@@ -35,18 +36,23 @@ function showWinner(winnerName) {
   winnerNameElement.textContent = winnerName;
   winnerElement.style.display = 'block';
   rollDiceButton.disabled = true;
+  resetButton.style.display = 'block';
 }
 function resetGame() {
     player1Score = 0;
     player2Score = 0;
+    num1=0;
+    num2=0;
     player1ScoreElement.textContent = '0';
     player2ScoreElement.textContent = '0';
     winnerElement.style.display = 'none';
     rollDiceButton.disabled = false;
-    rollDiceButton.style.display = 'block'; // Show the Roll Dice button
-    if (playAgainButton.parentElement === winnerElement) {
-      winnerElement.removeChild(playAgainButton); // Remove Play Again button
-    }
+    image1.setAttribute('src','assets/inverted-dice-6.png');
+    image2.setAttribute('src','assets/inverted-dice-6.png');
+    rollDiceButton.style.display = 'block';
+    resetButton.style.display = 'none';
+
 }
 
 rollDiceButton.addEventListener('click', rollDice);
+resetButton.addEventListener('click', resetGame);
